29d8066a2fb65276cc0a43fa07cbe2c70edac103
pd2 <its.pdp@gmail.com>
2025-07-16 10:51:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -307,7 +307,7 @@ document.getElementById("sharebutton").addEventListener("click", async () => {
     
     let shareText = document.getElementById("sharedata").innerHTML.replace(regex, "\n");
     
-    let linkURL = `https://pd2.github.io/numerale/`;
+    let linkURL = window.location.href;
     
     // shareText = `Numble #${answer} ${row}/6\n\n${shareText}`
     
